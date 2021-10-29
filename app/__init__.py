6128c0342f408de1c41f44c0af908526432e2344
Danilo Nascimento <daconnas.dcn@gmail.com>
2021-10-29 15:02:44 -0300

    CI configured

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/2")
+def hello_world2():
     return "<p>Hello, World!</p>"
