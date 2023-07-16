75eeaca7125f8f3df810174b6d0de8bbcb28bab0
Danilo da Conceição Nascimento <daconnas.dcn@gmail.com>
2023-07-16 12:18:52 -0300

    feat: adicionando tipos de dependencias da aplicação

---
+++ tcc/core/application/register/__init__.py
@@ -0,0 +1,2 @@
+from .register import Register  # noqa
+from .register_type import RegisterType  # noqa
--- tcc/core/application/register/register_type.py
+++ tcc/core/application/register/register_type.py
@@ -2,4 +2,6 @@ from enum import Enum
 
 
 class RegisterType(str, Enum):
-    ...
+    TABLE_REPOSITORY = "table_repository"
+    TRANSFORMER_RUNNER = "transformer_runner"
+    VARIATION_REPOSITORY = "variation_repository"
